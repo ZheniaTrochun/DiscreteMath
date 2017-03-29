5de87f92ab94ffa1d37515102afb20ea5d083f61
ZheniaTrochun <zheniatrochun@ukr.net>
2017-03-29 14:09:32 +0300

    splitted SI method

--- Server/handlers/arrUtils.js
+++ Server/handlers/arrUtils.js
@@ -1,6 +1,47 @@
 'use strict'
 
 class ArrUtils {
+  static getSumizhMatrix(vertexNum, edgesNum, edgesArr) {
+    let sumizhArr = [];
+    
+    for(let i = 0; i < vertexNum; i++) {
+      sumizhArr[i] = [];
+
+      for(let j = 0; j < vertexNum; j++) {
+        sumizhArr[i][j] = 0;
+      }
+    }
+
+    for(let j = 0; j < edgesNum; j++){
+      sumizhArr[edgesArr[j][0] - 1][edgesArr[j][1] - 1] = 1;
+    }
+
+    return sumizhArr;
+  }
+
+  static getIncindentMatrix(vertexNum, edgesNum, edgesArr) {
+    let incindentArr = [];
+
+    for(let i = 0; i < vertexNum; i++) {
+      incindentArr[i] = [];
+
+      for(let j = 0; j < edgesNum; j++) {
+        incindentArr[i][j] = 0;
+      }
+    }
+
+    for(let j = 0; j < edgesNum; j++) {
+      if(edgesArr[j][0] == edgesArr[j][1]) {
+        incindentArr[edgesArr[j][0] - 1][j] = 2;
+      } else {
+        incindentArr[edgesArr[j][0] - 1][j] = -1;
+        incindentArr[edgesArr[j][1] - 1][j] = 1;
+      }
+    }
+
+    return incindentArr;
+  }
+
   static getSI(request, response) {
 
     response.setHeader('Access-Control-Allow-Origin', '*');
@@ -14,38 +55,15 @@ class ArrUtils {
 
       let jsonStr = '';
 
-      let incindentArr = [],
-          sumizhArr = [];
+      let incindentArr, sumizhArr;
 
       request.on('data', (data) => jsonStr += data);
 
       request.on('end', () => {
           const reqData = JSON.parse(jsonStr);
 
-          for(let i = 0; i < reqData.vertexNum; i++){
-            incindentArr[i] = [];
-            sumizhArr[i] = [];
-
-            for(let j = 0; j < reqData.vertexNum; j++){
-              sumizhArr[i][j] = 0;
-            }
-
-            for(let j = 0; j < reqData.edgesNum; j++){
-              incindentArr[i][j] = 0;
-            }
-          }
-
-          for(let j = 0; j < reqData.edgesNum; j++){
-            sumizhArr[reqData.edgesArr[j][0] - 1][reqData.edgesArr[j][1] - 1] = 1;
-
-            if(reqData.edgesArr[j][0] == reqData.edgesArr[j][1]){
-              incindentArr[reqData.edgesArr[j][0] - 1][j] = 2;
-            }
-            else{
-              incindentArr[reqData.edgesArr[j][0] - 1][j] = -1;
-              incindentArr[reqData.edgesArr[j][1] - 1][j] = 1;
-            }
-          }
+          incindentArr = ArrUtils.getIncindentMatrix(reqData.vertexNum, reqData.edgesNum, reqData.edgesArr);
+          sumizhArr = ArrUtils.getSumizhMatrix(reqData.vertexNum, reqData.edgesNum, reqData.edgesArr);
 
           response.writeHead(200, { 'Content-Type': 'text/html'});
           response.write(JSON.stringify({
@@ -54,8 +72,7 @@ class ArrUtils {
           }));
           response.end();
       });
-    }
-    if(request.method == 'GET') {
+    } else {
       console.log('incorrect method');
       response.writeHead(500, { 'Content-Type': 'text/html'});
       response.end();
